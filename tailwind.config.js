ad15f6a797ed969a9df269fd7253a6dca5f634c1
sounabh <bagsounabh2003@gmail.com>
2025-06-03 10:28:34 +0530

    tailwind global styles issues

--- tailwind.config.js
+++ tailwind.config.js
@@ -1,17 +1,17 @@
 /** @type {import('tailwindcss').Config} */
 module.exports = {
   content: [
-    './pages/**/*.{js,ts,jsx,tsx,mdx}',
-    './components/**/*.{js,ts,jsx,tsx,mdx}',
-    './app/**/*.{js,ts,jsx,tsx,mdx}',
+    './app/**/*.{js,ts,jsx,tsx}',
+    './components/**/*.{js,ts,jsx,tsx}',
+    // Remove pages if not using pages dir
+    // './pages/**/*.{js,ts,jsx,tsx}',
   ],
+  darkMode: 'class', // if you use .dark class for dark mode
   theme: {
     extend: {
       fontFamily: {
-      
-        'apercu': ['var(--font-apercu)', 'sans-serif'],
-          'sfr': ['var(--font-sfr)', 'sans-serif'],
-        
+        apercu: ['var(--font-apercu)', 'sans-serif'],
+        sfr: ['var(--font-sfr)', 'sans-serif'],
       },
     },
   },
